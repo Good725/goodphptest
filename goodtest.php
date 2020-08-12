f37081566fbcc438efd7d5b694cb787f84bf14c1
Good725 <firly.larly0430@gmail.com>
2020-08-12 12:07:43 -0700

    test

---
+++ goodtest.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$db = "goodtest";
+	
+
+	$conn = mysqli_connect($servername, $username, $password, $db);
+    
+
+    $sql = "SELECT users.id, users.first_name, users.last_name, teams_users.team_id, teams.name, GROUP_CONCAT( teams.name ) as 'names' FROM users left join teams_users on users.id = teams_users.user_id left join teams on teams_users.team_id = teams.id group by users.id";
+ 	$result = $conn->query($sql);
+
+
+ 	while($row = $result->fetch_assoc()) {
+		$array[] = array('id'=>$row["id"], 'Name'=>$row["first_name"]." ".$row["last_name"], 'Teams'=>$row["names"]);
+	}
+
+?>
+
+<head>
+	<title>
+		The Good Test
+	</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	<style>
+		.centered {
+    		margin-top: 5%;
+		}
+		.text {
+  			margin-top: 10%;
+		}
+	</style>
+</head>
+<body>
+	<div class="container text" style="text-align: center;">
+		<h1>
+			!!! Thank you for providing the test !!!
+		</h1>
+	</div>
+	<div class="container centered">
+		<table class="table table-bordered">
+			<tr>
+				<?php foreach($array[0] as $key=>$value){ ?>
+			            <th class="col-sm" style="font-size: 30px"><?php echo $key; ?></th>
+				<?php } ?>
+			</tr>
+			<?php foreach( $array as $key=>$value){ ?>
+				<tr>
+					<?php foreach($value as $key2=>$value2){ ?>
+						<td class="col-sm" style="font-size: 20px">
+							<?php echo $value2; ?>
+						</td>
+					<?php }?>
+				</tr>
+		    <?php } ?>
+		</table>
+	</div>
+		
+</body>
+</html>
+
+
+
+
+
+ 	 		
